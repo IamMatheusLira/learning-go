e061c188b54bc61b94dd89d98cbb3284f5998569
Matheus Lira <61715822+IamMatheusLira@users.noreply.github.com>
2022-11-28 18:46:59 -0300

    fix: create packages to solve redeclared main

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "example/hello/mypackage"
+import (
+	"example/hello/mypackage"
+	"fmt"
+)
 
 func main() {
+  fmt.Println("Hello World") 
   mypackage.SayGreetings("Matheus")
 }
